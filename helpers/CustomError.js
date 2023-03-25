29b4f07aa91fcf38d97fac2abf440059746a2ed2
Karam Gbaren <karam99.k@hotmail.com>
2023-03-26 02:02:44 +0300

    Updates for handling errors and editing campground and creating a new one

---
+++ helpers/CustomError.js
@@ -0,0 +1,10 @@
+class CustomError extends Error{
+    constructor(message,statusCode){
+        super();
+        this.message=message;
+        this.statusCode=statusCode;
+    }
+}
+
+
+module.exports=CustomError;
